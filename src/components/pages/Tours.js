89bd5bb826acd14fab18d351e2fbbe436fd89b43
Jinok <joseo46@hotmail.com>
2021-07-28 16:50:24 +1000

    add tours card

--- src/components/pages/Tours.js
+++ src/components/pages/Tours.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import './Tours.css';
-import TextCard from '../TextCard';
+import TourItem from '../TourItem';
+import Content from '../Content';
 function Tours() {
   return (
-    <section className='section-tours mx-5'>
-      <div className='row'>
-        <div className='col-lg-4'>
-          <TextCard />
-        </div>
-        <div className='col-lg-4'>
-          <TextCard />
-        </div>
-        <div className='col-lg-4'>
-          <TextCard />
-        </div>
+    <section className='section-tours mx-5 '>
+      <div className='d-grid'>
+        <div className='row '>
+          <Content
+            classname='mb__large'
+            heading='Most popular tours'
+            sizes='secondary'
+            align='center'
+            initType={true}
+            dataAos='fade-up'
+            dataDelay=''
+          />
 
-        {/* <h2 className='heading-secondary'>Most popular tours</h2> */}
+          <div className='section__tours--box d-flex'>
+            <div className='col-lg-4 p-3'>
+              <TourItem src='/images/Milford_sound.jpg' alt='Milford sound' />
+            </div>
+            <div className='col-lg-4 p-3'>
+              <TourItem
+                src='/images/Tongariro_National_Park.jpg'
+                alt='Tongariro National Park'
+              />
+            </div>
+            <div className='col-lg-4 p-3'>
+              <TourItem src='/images/forest.jpg' alt='Forest' />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
